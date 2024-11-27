a142ed1cfb0cd91e75b5ab46bc07ca52cc6a0402
Glen Dsouza <samuel.george@waycool.in>
2024-11-27 11:15:58 +0530

    crud operations api for category, expenses , expense summary of user based on date filter apis

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ExpenseController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +16,22 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::post('/login', function (Request $request) {
+    $credentials = $request->only('email', 'password');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    if (Auth::attempt($credentials)) {
+        $user = Auth::user();
+        return response()->json([
+            'token' => $user->createToken('auth-token')->plainTextToken
+        ]);
+    }
+
+    return response()->json(['message' => 'Unauthorized'], 401);
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('categories', CategoryController::class);
+    Route::apiResource('expenses', ExpenseController::class);
+    Route::post('expenses/summary', [ExpenseController::class, 'summary']);
+
 });
